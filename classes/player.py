7d4ad6e57d2e70586d6f5d811273473f08814593
DanielGallegoRuiz <gallegoruizdaniel@gmail.com>
2021-05-19 23:20:39 +0200

    Modificamos el jugador cargando un sprite

--- classes/player.py
+++ classes/player.py
@@ -3,46 +3,37 @@ import pygame
 
 class Player:
     def __init__(self):
-        self.x = 380
-        self.y = 370
-        self.ancho = 30
-        self.largo = 90
         self.max_abajo = 370
         self.max_arriba = 37
-        self.gravity = 8
+        self.gravity = 6
         self.force = 5
+        self.player = pygame.image.load("sprites/player.png")
+        self.player_rect = self.player.get_rect()
+        self.player_rect.x = 380
+        self.player_rect.y = 370
 
     def draw(self, screen):
-        pygame.draw.rect(
-            screen,
-            (130, 229, 0),
-            pygame.Rect(
-                self.x,
-                self.y,
-                self.ancho,
-                self.largo,
-            )
-        )
+        screen.blit(self.player, self.player_rect)
 
     def __check_limits(self):
         """
         Controlamos que no se pase del ux
         """
-        if self.y > self.max_abajo:
-            self.y = self.max_abajo
-        elif self.y < self.max_arriba:
-            self.y = self.max_arriba
+        if self.player_rect.y > self.max_abajo:
+            self.player_rect.y = self.max_abajo
+        elif self.player_rect.y < self.max_arriba:
+            self.player_rect.y = self.max_arriba
 
     def movement(self, key_pressed):
         """
         Al pulsar espacio movemos el rectangulo y le aplicamos gravedad
         :param key_pressed: pygame.key.get_pressed()
         """
-        if self.max_abajo >= self.y >= self.max_arriba:
+        if self.max_abajo >= self.player_rect.y >= self.max_arriba:
             if key_pressed[pygame.K_SPACE]:
-                self.y -= self.force
+                self.player_rect.y -= self.force
             else:
                 # Le ponemos gravedad al rectangulo
-                if self.y < self.max_abajo:
-                    self.y += self.gravity
+                if self.player_rect.y < self.max_abajo:
+                    self.player_rect.y += self.gravity
         self.__check_limits()
